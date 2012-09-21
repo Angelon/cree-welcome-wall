5af1376fa514408ae92c2d6a7460040050e84513
Carter Corker <cartercorker@gmail.com>
2012-09-21 01:48:35 -0400

    First update

--- js/script.js
+++ js/script.js
@@ -10,6 +10,9 @@ $(document).ready(function (){
 
 function flipTile(element){
 	console.log("Firing transition");
+	//console.log($(element));
+	var myArticleNumber = $(element).attr("article");
+	console.log($("articlecontainer[article="+myArticleNumber+"] tile"));
 
 	if($(element).hasClass("flipped")){
 		
@@ -21,43 +24,121 @@ function flipTile(element){
 
 	$(element).transition({
 		rotateX: '180deg',
-		perspective: '100px'
+		perspective: '1000px'
 	}, function (){
-		$("tile").unbind();
-		console.log($(this).children("face.back").children("tilefacecontent").children(".readmore"));
-		$(this).children("face.back").children("tilefacecontent").children(".readmore").css({"z-index":8000});
-		$(element).parents("tilegroupcontainer").click(function(){
-			console.log("Fipping...");
+	//console.log($(element).children("face.back").children("tilefacecontent").offset());
+	//console.log($(element).children("face.back").children("tilefacecontent").width());
+	//console.log($(element).children("face.back").children("tilefacecontent").height());
+
+	var topPos = ($(element).children("face.back").children("tilefacecontent").offset().top + $(element).children("face.back").children("tilefacecontent").height()) - 30;
+	var leftPos = ($(element).children("face.back").children("tilefacecontent").offset().left + $(element).children("face.back").children("tilefacecontent").width()) - 350;
+	var readMoreButton = $(".article-button.readmore[article="+myArticleNumber+"]");
+	var closeButton = $(".article-button.close[article="+myArticleNumber+"]");
+
+	readMoreButton.offset({top:topPos, left: leftPos});
+	readMoreButton.fadeIn();
+		//Remove all click events from tiles
+		$("articlecontainer[article="+myArticleNumber+"] tile").unbind();
+
+		readMoreButton.click(function(){
+			$(".article-button.readmore").hide();
+			$(".article-button.close").hide();
+			
+			//On click, flip the tile over again
 			$(element).transition({
 				rotateX: '0deg',
 				perspective: '100px'
 			}, function (){
-				$("tile").css({"-webkit-transform-style": "flat"});
-				flipArticle($(this).attr("article"));
+				$("articlecontainer[article="+myArticleNumber+"] tile").css({"-webkit-transform-style": "flat"});
+				console.log($(element).attr("articleSource"));
+				$("articlecontainer[article="+myArticleNumber+"] articleface.back iframe").attr("src", $(element).attr("articleSource"));
+				flipArticle($(element).attr("article"));
 			});
 			
 			
+		});
+
+		closeButton.offset({top:topPos, left: leftPos+200});
+		closeButton.fadeIn();
+
+		closeButton.click(function (){
+			
+			resetTile(element, myArticleNumber);
 		});
 	});
 
 	
 }
 
-
-function flipArticle(articleNumber){
-	$("tile").removeClass("flipped");
-	
-
-	console.log("Flipping Article: " + articleNumber);
-	$("articlecontainer").show();
-	$("articlecontainer").transition({
-		rotateY: '180deg',
-		perspective: '1000px'
-	}, 500, "linear", function(){
-		//$(this).find("tilegroup").hide();
-		$("tile").removeClass("flipped");
+function resetTile(element, articleNumber){
+	console.log("Resetting Tile");
+	console.log($(element));
+	if($(element).hasClass("flipped")){
+		$(element).removeClass("flipped");
+	}
+	else {
 		
 	}
-	);
+
+	$(".article-button.readmore").offset({top:0, left: 0});
+	$(".article-button.readmore").hide();
+	$(".article-button.readmore").unbind();
+
+
+	$(".article-button.close").offset({top:0, left: 0});
+	$(".article-button.close").hide();
+	$(".article-button.close").unbind();
+
+	
+
+	$(element).transition({
+		rotateX: '0deg',
+		perspective: '100px'
+	}, function (){
+		$("articlecontainer[article="+articleNumber+"] tilecontainer").css({"z-index":""});
+		bindTiles(articleNumber);
+	});
+}
+
+
+function flipArticle(articleNumber){
+	console.log("Showing article: " + articleNumber);
+	$("articlecontainer[article="+articleNumber+"] tile").removeClass("flipped");
+
+	$("articlecontainer[article="+articleNumber+"]").show();
+	$("articlecontainer[article="+articleNumber+"]").transition({
+		rotateY: '180deg',
+		perspective: '1000px'
+	}, 500,  function(){
+		var topPos = $(this).height() - 500;
+		var leftPos = $(this).width();
+		$(".article-button.close").offset({top: topPos, left: leftPos});
+		$(".article-button.close").fadeIn();
+		$(".article-button.close").unbind();
+		$(".article-button.close").click(function (){
+			resetArticle(articleNumber);
+		});
+		
+	});
+}
+
+function resetArticle(articleNumber){
+	$(".article-button.close").hide();
+	$(".article-button.close").unbind();
+	$("articlecontainer[article="+articleNumber+"]").transition({
+		rotateY: '0deg',
+		perspective: '1000px'
+	}, 500,  function(){
+		$("articlecontainer[article="+articleNumber+"] tilecontainer").css({"z-index":""});
+		bindTiles(articleNumber);
+		
+	});
+}
+
+function bindTiles(articleNumber){
+	$("articlecontainer[article="+articleNumber+"] tile").css({"-webkit-transform-style": "preserve-3d"}); 
+	$("articlecontainer[article="+articleNumber+"] tile").click(function (){
+			flipTile($(this));
+	});
 }
 
